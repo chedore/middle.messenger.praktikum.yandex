49b0554d3da807351095758667662cb352fb4e17
Svetlana Prozhirova <svetakesha@yandex.ru>
2024-01-07 18:26:28 +0300

    feat: верстка страницы chat

--- src/components/chat/__card-list/index.ts
+++ src/components/chat/__card-list/index.ts
@@ -2,9 +2,10 @@ import Handlebars from 'handlebars';
 export { default as ListChatCards } from './chat__card-list.hbs?raw';
 
 Handlebars.registerHelper('chats', () => {
-    return [
-        {name: 'cat-1', avatar: 'cat1'},
-        {name: 'cat-2', avatar: 'cat2'},
-        {name: 'cat-3', avatar: 'cat3'},
-    ]
+  return [
+    {name: 'Андрей', message: 'Изображение', time:"10:49", count:'2'},
+    {name: 'Киноклуб', message: 'стикер', owner:true, time:"12:00"},
+    {name: 'Илья', message: 'Друзья, у меня для вас особенный выпуск новостей!...', time:"15:12", count:'4'},
+    {name: 'Вадим', message: 'Круто!', owner:true, time:"Пт"},
+  ]
 })
