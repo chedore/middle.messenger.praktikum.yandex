f8728641292760729280f36a63456c3819bd1535
Svetlana Prozhirova <svetakesha@yandex.ru>
2024-01-08 11:53:12 +0300

    feat: верстка страницы list

--- src/components/reference/__links/index.ts
+++ src/components/reference/__links/index.ts
@@ -1,1 +1,10 @@
+import Handlebars from 'handlebars';
 export { default as ReferenceLinks } from './reference__links.hbs?raw';
+
+Handlebars.registerHelper('pages', () => {
+  return [
+    {page: 'login'},
+    {page: 'register'},
+    {page: 'chat'},
+  ]
+})
