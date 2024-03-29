a554f47135434092a769cd6a419cef08a8ffe69a
Svetlana Prozhirova <svetakesha@yandex.ru>
2024-03-29 12:00:24 +0300

    fix: тест2

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,18 +3,13 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-    'plugin:@typescript-eslint/recommended',
-  ],
+  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   // overrides: [
   //   {
   //     env: {
@@ -32,11 +27,18 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     'import/prefer-default-export': 'off',
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    'import/extensions': ['error', 'ignorePackages', {
-      js: 'never',
-      ts: 'never',
-    }],
-    'no-underscore-dangle': ['error', { allow: ['__dirname'], allowAfterThis: true }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+      },
+    ],
+    'no-underscore-dangle': [
+      'error',
+      { allow: ['__dirname'], allowAfterThis: true },
+    ],
     'import/no-unresolved': [2, { ignore: ['\\?raw$'] }],
     'class-methods-use-this': 'off',
     '@typescript-eslint/no-this-alias': 'off',
@@ -48,5 +50,4 @@ module.exports = {
       },
     },
   },
-
 };
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
-  plugins: [
-    handlebars()
-  ],
+  plugins: [handlebars()],
   build: {
-    outDir: 'dist'
-  }
-
-})
+    outDir: 'dist',
+  },
+});
