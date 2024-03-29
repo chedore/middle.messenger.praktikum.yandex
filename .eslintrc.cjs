5fb8e2e3b6cf9c1236b6a7734ddb52df498fca96
Svetlana Prozhirova <svetakesha@yandex.ru>
2024-03-29 20:27:49 +0300

    тест2

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,23 +3,50 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
-  rules: {},
+  plugins: [
+    '@typescript-eslint',
+  ],
+  // overrides: [
+  //   {
+  //     env: {
+  //       node: true,
+  //     },
+  //     files: ['.eslintrc.{js,cjs}'],
+  //     parserOptions: {
+  //       sourceType: 'script',
+  //     },
+  //   },
+  // ],
+  rules: {
+    'max-len': [2, 150],
+    'max-params': [2, 3],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never',
+      ts: 'never',
+    }],
+    'no-underscore-dangle': ['error', { allow: ['__dirname'], allowAfterThis: true }],
+    'import/no-unresolved': [2, { ignore: ['\\?raw$'] }],
+    'class-methods-use-this': 'off',
+    '@typescript-eslint/no-this-alias': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts', '.hbs'],
+      },
+    },
+  },
+
 };
